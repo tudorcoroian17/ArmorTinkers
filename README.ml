6ed3ae06e82ba274cb8deff532ae2d3322d070a2
Tudor Coroian <tudorcoroian17w@gmail.com>
2022-04-20 17:04:05 +0300

    Update README.ml

--- README.ml
+++ README.ml
@@ -1,1 +1,4 @@
-I cannot take it anymore
+==========Structure==========
+	- Documentation folder contains the final chapters of the thesis
+	- Bits&Pieces contains information gathered along the way of implementation
+	- ProjectFld contains the source code for the mod
